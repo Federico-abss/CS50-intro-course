c79a32e61cfb253aa5cd3d4c86042b11cb5328da
Federico-abss <federicomannucci@yahoo.it>
2020-01-28 10:42:57 +0100

    Corrected problem caused from hardcoding dna lines

--- Python/pset6/dna/dna.py
+++ Python/pset6/dna/dna.py
@@ -9,57 +9,48 @@ with open(argv[2]) as dnafile:
 
 # store it in a string
 dna = dnalist[0]
-AGATC = 0
-AATG = 0
-TATC = 0
-# create a dictionary with the sequences we intend to count
-sequences = {
-    "AGATC": 1,
-    "AATG": 1,
-    "TATC": 1
-}
+# create a dictionary where we will store the sequences we intend to count
+sequences = {}
+
+with open(argv[1]) as peoplefile:
+    people = reader(peoplefile)
+    for row in people:
+        dnaSequences = row
+        dnaSequences.pop(0)
+        break
+
+for item in dnaSequences:
+    sequences[item] = 1
 
 # iterate trough the dna sequence, when it finds repetitions of the values from sequence dictionary it counts them
-for i in range(len(dna)):
-    temp = 0
+for key in sequences:
+    l = len(key)
+    tempMax = 0
+    for i in range(len(dna)):
+        temp = 0
+        # if the segment of dna corresponds to the key and there is a repetition of it we start counting
+        if dna[i: i + l] == key:
+            while dna[i - l: i] == dna[i: i + l]:
+                temp += 1
+                i += l
+                # it compares the value to the previous longest sequence and if it is longer it overrides it
+                if temp > tempMax:
+                    tempMax = temp
 
-    if dna[i: i + 4] == "AATG":
-        while dna[i - 4: i] == dna[i: i + 4]:
-            temp += 1
-            i += 4
-            # it compares the value to the previous longest sequence and if it is longer it overrides it
-            if temp > AATG:
-                AATG = temp
-
-    elif dna[i: i + 4] == "TATC":
-        while dna[i - 4: i] == dna[i: i + 4]:
-            temp += 1
-            i += 4
-            if temp > TATC:
-                TATC = temp
-
-    elif dna[i: i + 5] == "AGATC":
-        while dna[i - 5: i] == dna[i: i + 5]:
-            temp += 1
-            i += 5
-            if temp > AGATC:
-                AGATC = temp
-
-# store the longest sequences in the dictionary
-sequences["AGATC"] += AGATC
-sequences["AATG"] += AATG
-sequences["TATC"] += TATC
+    # store the longest sequences in the dictionary using the correspondent key
+    sequences[key] += tempMax
 
 # open and iterate trough the database of people treating each one like a dictionary so it can compare to the sequences one
 with open(argv[1], newline='') as peoplefile:
     people = DictReader(peoplefile)
     for person in people:
         match = 0
-        # compares the three sequences to every person and prints name before leaving the program if there is a match
+        # compares the sequences to every person and prints name before leaving the program if there is a match
         for dna in sequences:
             if sequences[dna] == int(person[dna]):
                 match += 1
-        if match == 3:
+        if match == len(sequences):
             print(person['name'])
             exit()
+
     print("No match")
